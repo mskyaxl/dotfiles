2e9fc884f3fff0cf7f1cdd6d5270535ef2c6b020
Alexandru Smochina <ASmochina@luxoft.com>
2018-10-11 14:58:36 +0200

    vim:check if the solarised theme is installed first;fixed aptget config;

--- aptget/install_packages.sh
+++ aptget/install_packages.sh
@@ -4,4 +4,6 @@ set -e
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-${BASEDIR}/install -p ${BASEDIR}/aptget/dotbot_plugin_aptget/aptget.py -c ${BASEDIR}/aptget/packages.conf.yaml
+sudo ${BASEDIR}/install -p ${BASEDIR}/aptget/dotbot_plugin_aptget/aptget.py -c ${BASEDIR}/aptget/packages.conf.yaml
+sudo -K
+
--- zsh/make_zsh_default.sh
+++ zsh/make_zsh_default.sh
@@ -1,4 +1,6 @@
-   # If this user's login shell is not already "zsh", attempt to switch.
+#!/usr/bin/env bash
+
+# If this user's login shell is not already "zsh", attempt to switch.
    TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
    if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
       #If this platform provides a "chsh" command (not Cygwin), do it, man!
@@ -10,4 +12,6 @@
          printf "I can't change your shell automatically because this system does not have chsh.\n"
          printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
       fi
+   else
+     printf "Cool! zsh is already the default shell!\n"	   
    fi
