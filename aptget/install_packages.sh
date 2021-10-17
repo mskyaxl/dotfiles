daa23400b2046235b6dc86742f58915616564190
Alex Mosky <mskyaxl@protonmail.com>
2021-10-17 22:24:19 +0200

    introduce profiles in order to be able to maintain targets (#2)
    create profiles:
    * basic(already existing packages) - to be used for RPI
    * ubuntu that adds alacritty beside basic
    
    Co-authored-by: Alex Mosky <mosky@pop-os.localdomain>

--- aptget/install_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
-sudo ${BASEDIR}/install -p ${BASEDIR}/aptget/dotbot_plugin_aptget/aptget.py -c ${BASEDIR}/aptget/packages.conf.yaml
-sudo -K
-
