5508ae4b3d751912e3c6f35d79b921d6e4582deb
Alex Mosky <mskyaxl@protonmail.com>
2022-08-06 14:21:27 +0200

    add script to intsall firacode

---
+++ alacritty/install_firacode_nerd_font.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+NERD_FONT_INSTALL_DIR=~/.local/share/fonts/NerdFonts
+FIRACODE_NERD_FONT_GITHUB=https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode
+
+mkdir -p "${NERD_FONT_INSTALL_DIR}"
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Bold Nerd Font Complete Mono Windows Compatible.ttf"      ${FIRACODE_NERD_FONT_GITHUB}/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Bold Nerd Font Complete Mono.ttf"                         ${FIRACODE_NERD_FONT_GITHUB}/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Bold Nerd Font Complete Windows Compatible.ttf"           ${FIRACODE_NERD_FONT_GITHUB}/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Bold Nerd Font Complete.ttf"                              ${FIRACODE_NERD_FONT_GITHUB}/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete.ttf
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Light Nerd Font Complete Mono Windows Compatible.ttf"     ${FIRACODE_NERD_FONT_GITHUB}/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Light Nerd Font Complete Mono.ttf"                        ${FIRACODE_NERD_FONT_GITHUB}/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Light Nerd Font Complete Windows Compatible.ttf"          ${FIRACODE_NERD_FONT_GITHUB}/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Light Nerd Font Complete.ttf"                             ${FIRACODE_NERD_FONT_GITHUB}/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete.ttf
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Medium Nerd Font Complete Mono Windows Compatible.ttf"    ${FIRACODE_NERD_FONT_GITHUB}/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Medium Nerd Font Complete Mono.ttf"                       ${FIRACODE_NERD_FONT_GITHUB}/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Medium Nerd Font Complete Windows Compatible.ttf"         ${FIRACODE_NERD_FONT_GITHUB}/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Medium Nerd Font Complete.ttf"                            ${FIRACODE_NERD_FONT_GITHUB}/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Regular Nerd Font Complete Mono Windows Compatible.ttf"   ${FIRACODE_NERD_FONT_GITHUB}/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Regular Nerd Font Complete Mono.ttf"                      ${FIRACODE_NERD_FONT_GITHUB}/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Regular Nerd Font Complete Windows Compatible.ttf"        ${FIRACODE_NERD_FONT_GITHUB}/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Regular Nerd Font Complete.ttf"                           ${FIRACODE_NERD_FONT_GITHUB}/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Retina Nerd Font Complete Mono Windows Compatible.ttf"    ${FIRACODE_NERD_FONT_GITHUB}/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Retina Nerd Font Complete Mono.ttf"                       ${FIRACODE_NERD_FONT_GITHUB}/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Retina Nerd Font Complete Windows Compatible.ttf"         ${FIRACODE_NERD_FONT_GITHUB}/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code Retina Nerd Font Complete.ttf"                            ${FIRACODE_NERD_FONT_GITHUB}/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete.ttf
+
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code SemiBold Nerd Font Complete Mono Windows Compatible.ttf"  ${FIRACODE_NERD_FONT_GITHUB}/SemiBold/complete/Fira%20Code%20SemiBold%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code SemiBold Nerd Font Complete Mono.ttf"                     ${FIRACODE_NERD_FONT_GITHUB}/SemiBold/complete/Fira%20Code%20SemiBold%20Nerd%20Font%20Complete%20Mono.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code SemiBold Nerd Font Complete Windows Compatible.ttf"       ${FIRACODE_NERD_FONT_GITHUB}/SemiBold/complete/Fira%20Code%20SemiBold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/Fira Code SemiBold Nerd Font Complete.ttf"                          ${FIRACODE_NERD_FONT_GITHUB}/SemiBold/complete/Fira%20Code%20SemiBold%20Nerd%20Font%20Complete.ttf
+
+fc-cache -fv
