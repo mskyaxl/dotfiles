da842f51710bfc0cd6c57f8cb5288447c3805697
Alex Mosky <mskyaxl@protonmail.com>
2023-12-19 12:48:17 +0100

    [alacritty] fix font installation

--- alacritty/install_hack_nerd_font.sh
+++ alacritty/install_hack_nerd_font.sh
@@ -5,13 +5,17 @@ HACK_NERD_FONT_GITHUB=https://github.com/ryanoasis/nerd-fonts/raw/master/patched
 
 mkdir -p "${NERD_FONT_INSTALL_DIR}"
 
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Regular Nerd Font Complete Mono.ttf"     ${HACK_NERD_FONT_GITHUB}/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Regular Nerd Font Complete.ttf"          ${HACK_NERD_FONT_GITHUB}/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete%20Mono.ttf  
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Bold Nerd Font Complete Mono.ttf"        ${HACK_NERD_FONT_GITHUB}/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf  
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Bold Nerd Font Complete.ttf"             ${HACK_NERD_FONT_GITHUB}/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Italic Nerd Font Complete.ttf"           ${HACK_NERD_FONT_GITHUB}/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete%20Mono.ttf 
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Italic Nerd Font Complete Mono.ttf"      ${HACK_NERD_FONT_GITHUB}/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Bold Italic Nerd Font Complete Mono.ttf" ${HACK_NERD_FONT_GITHUB}/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
-curl -fLo "${NERD_FONT_INSTALL_DIR}/Hack Bold Italic Nerd Font Complete.ttf"      ${HACK_NERD_FONT_GITHUB}/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf     
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFont-Bold.ttf"            ${HACK_NERD_FONT_GITHUB}/Bold/HackNerdFont-Bold.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontMono-Bold.ttf"        ${HACK_NERD_FONT_GITHUB}/Bold/HackNerdFontMono-Bold.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontPropo-Bold.ttf"       ${HACK_NERD_FONT_GITHUB}/Bold/HackNerdFontPropo-Bold.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFont-BoldItalic.ttf"      ${HACK_NERD_FONT_GITHUB}/BoldItalic/HackNerdFont-BoldItalic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontMono-BoldItalic.ttf"  ${HACK_NERD_FONT_GITHUB}/BoldItalic/HackNerdFontMono-BoldItalic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontPropo-BoldItalic.ttf" ${HACK_NERD_FONT_GITHUB}/BoldItalic/HackNerdFontPropo-BoldItalic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFont-Italic.ttf"          ${HACK_NERD_FONT_GITHUB}/Italic/HackNerdFont-Italic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontMono-Italic.ttf"      ${HACK_NERD_FONT_GITHUB}/Italic/HackNerdFontMono-Italic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontPropo-Italic.ttf"     ${HACK_NERD_FONT_GITHUB}/Italic/HackNerdFontPropo-Italic.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontMono-Regular.ttf"     ${HACK_NERD_FONT_GITHUB}/Regular/HackNerdFontMono-Regular.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFontPropo-Regular.ttf"    ${HACK_NERD_FONT_GITHUB}/Regular/HackNerdFontPropo-Regular.ttf
+curl -fLo "${NERD_FONT_INSTALL_DIR}/HackNerdFont-Regular.ttf"         ${HACK_NERD_FONT_GITHUB}/Regular/HackNerdFont-Regular.ttf
 
 fc-cache -fv
