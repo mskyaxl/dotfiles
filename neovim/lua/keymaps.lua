454fe9930c68f96e0a0d9924407c427fd2bb8aeb
Alex Mosky <mskyaxl@protonmail.com>
2024-10-11 10:28:55 +0200

    [nvim] add keymap to deactivate diag messages

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -13,7 +13,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
+vim.keymap.set('n', '<leader>dd', function() 
+  vim.diagnostic.enable(false)
+end, { desc = 'Disable diagnostic current buffer'})
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
