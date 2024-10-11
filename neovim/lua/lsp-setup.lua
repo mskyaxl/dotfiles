a42953e1227e18ce97987515cc67cdd41bcedc47
Alex Mosky <mskyaxl@protonmail.com>
2024-10-11 10:31:19 +0200

    [nvim] update which-key config

--- neovim/lua/lsp-setup.lua
+++ neovim/lua/lsp-setup.lua
@@ -47,23 +47,26 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+local wk = require("which-key")
+wk.add({
+  {"<leader>c", desc = '[C]ode' },
+  {"<leader>d", desc = '[D]ocument' },
+  {"<leader>g", desc = '[G]it' },
+  {"<leader>h", desc = 'Git [H]unk' },
+  {"<leader>r", desc = '[R]ename' },
+  {"<leader>s", desc = '[S]earch' },
+  {"<leader>t", desc = '[T]oggle' },
+  {"<leader>w", desc = '[W]orkspace' },
+
+})
+
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
-
+wk.add({
+  { mode = 'v' },
+  {"<leader>", desc = 'VISUAL <leader>' },
+  {"<leader>", desc = 'Git [H]unk' },
+})
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
