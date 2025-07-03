aec6c25f90bdad699e1bb067b00754c6b48214df
Alex Mosky <mskyaxl@protonmail.com>
2025-07-03 08:06:26 +0200

    [nvim] add  deactivate copilot for now

--- neovim/lua/lazy-plugins.lua
+++ neovim/lua/lazy-plugins.lua
@@ -62,7 +62,7 @@ require('lazy').setup({
   -- session management
   require('plugins/prosession').goLazy(),
 
-  require('plugins/copilot').goLazy(),
+ -- require('plugins/copilot').goLazy(),
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -6,13 +6,25 @@ function M.goLazy()
     {
       "CopilotC-Nvim/CopilotChat.nvim",
       dependencies = {
-        { "github/copilot.vim" },                     -- or zbirenbaum/copilot.lua
-        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        -- { "github/copilot.vim" },                     -- or zbirenbaum/copilot.lua
+        {
+          "zbirenbaum/copilot.lua",
+          cmd = "Copilot",
+          event = "InsertEnter",
+          config = function()
+            require("copilot").setup({})
+          end,
+        },
+        { "nvim-lua/plenary.nvim", branch = "master" },     -- for curl, log and async functions
       },
-      build = "make tiktoken",                        -- Only on MacOS or Linux
+      build = "make tiktoken",                              -- Only on MacOS or Linux
       opts = {
         -- See Configuration section for options
       },
+      config = function()
+        -- return ':Copilot disable'
+            require("CopilotChat").setup({})
+      end,
       -- See Commands section for default commands if you want to lazy load on them
     },
   }
